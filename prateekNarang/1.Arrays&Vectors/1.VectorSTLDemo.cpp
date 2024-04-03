c8e5da5169d8901a240d373cc4708f5bc7401f0f
amarvenna <amareswarareddyvenna2001@gmail.com>
2024-04-03 16:37:03 +0530

    file added

--- prateekNarang/1.Arrays&Vectors/1.VectorSTLDemo.cpp
+++ prateekNarang/1.Arrays&Vectors/1.VectorSTLDemo.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-        
+     // fill constructor
+     vector<int> arr(10,0);
+     arr.push_back(11);
+     arr.push_back(12);
+     arr.pop_back();
+     cout<<endl;
 
-
-
-	return 0;
+     for(int i=0;i<arr.size();i++){
+	     cout<<arr[i]<<" ";
+     }
+     cout<<endl;
+     return 0;
 }
